ff5d19877dbfa640bcd903b4214a4184d763a0e2
gokalpgokce <gokalpgokce7@gmail.com>
2021-12-07 00:03:51 +0300

    Add Move and Fire function

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,29 +7,30 @@ public class Player : MonoBehaviour
     public GameObject bullet;
     public GameObject firePoint;
     private float bulletSpeed = 1000f;
-    public CharacterController _controller;
+    public CharacterController controller;
     private float playerSpeed = 10f;
-    void Start()
+    
+    void Update()
     {
-        
+        Move();
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Fire();
+        }
+    }
+    
+    void Fire()
+    {
+        GameObject tempBullet = Instantiate(bullet, firePoint.transform.position, Quaternion.identity);
+        tempBullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
     }
 
-    void Update()
+    void Move()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if (direction.magnitude >= 0.1f)
-        {
-            _controller.Move(direction * playerSpeed * Time.deltaTime);
-        }
-        
-        
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            GameObject tempBullet = Instantiate(bullet, firePoint.transform.position, Quaternion.identity);
-            tempBullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
-        }
+        controller.Move(direction * playerSpeed * Time.deltaTime);
     }
 }
